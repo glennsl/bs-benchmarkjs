75faf871fab39f0cd86f9892d06ff4a8c2a2304b
Alain Armand <aarmand.inbox@gmail.com>
2021-03-13 21:35:46 +0100

    add implementation file, reason syntax example

--- src/benchmark.mli
+++ src/benchmark.mli
@@ -2,6 +2,6 @@
 type suite
 
 external makeSuite : string -> suite = "Suite" [@@bs.new] [@@bs.module "benchmark"]
-external add : string -> (unit -> unit) -> suite = "" [@@bs.send.pipe: suite]
-external on : string -> (< .. > Js.t -> unit) -> suite = "" [@@bs.send.pipe: suite]
-external run : unit -> unit = "" [@@bs.send.pipe: suite]
+external add : string -> (unit -> unit) -> suite = "add" [@@bs.send.pipe: suite]
+external on : string -> (< .. > Js.t -> unit) -> suite = "on" [@@bs.send.pipe: suite]
+external run : unit -> unit = "run" [@@bs.send.pipe: suite]
