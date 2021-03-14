426450fe53093233b57572de3756889afe8b6413
Glenn Slotte <glenn@slotte.net>
2021-03-14 12:59:10 +0100

    Delete benchmark.mli

--- src/benchmark.mli
+++
@@ -1,7 +0,0 @@
-
-type suite
-
-external makeSuite : string -> suite = "Suite" [@@bs.new] [@@bs.module "benchmark"]
-external add : string -> (unit -> unit) -> suite = "add" [@@bs.send.pipe: suite]
-external on : string -> (< .. > Js.t -> unit) -> suite = "on" [@@bs.send.pipe: suite]
-external run : unit -> unit = "run" [@@bs.send.pipe: suite]
